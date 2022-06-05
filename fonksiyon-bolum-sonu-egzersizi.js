bc5378c220941511d2c704d2f44cf612adb82b0d
Burak Obuz <burak.obuz02@gmail.com>
2022-06-05 17:29:30 +0300

    commit

---
+++ fonksiyon-bolum-sonu-egzersizi.js
@@ -0,0 +1,23 @@
+// Fonksiyon bolum sonu egzersizi
+
+let counter = 0
+let counterDOM = document.querySelector('#counter')
+let increaseDOM = document.querySelector('#increase')
+let deacreaseDOM = document.querySelector('#decrease')
+
+counterDOM.innerHTML = counter
+
+increaseDOM.addEventListener("click", clickEvent)
+deacreaseDOM.addEventListener("click", clickEvent)
+
+function clickEvent() {
+    console.log(this.id)
+    if (this.id == "increase") {
+
+        counterDOM.innerHTML = counter += 1
+    } else {
+        counterDOM.innerHTML = counter -= 1
+    }
+
+
+}
